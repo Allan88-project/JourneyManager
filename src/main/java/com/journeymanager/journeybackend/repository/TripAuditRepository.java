f05439bedbb91485441be5acdb7e874b2c63d43e
JourneyManager <yklee88@hotmail.com>
2026-03-13 07:05:03 +0800

    Add TripAuditRepository

---
+++ src/main/java/com/journeymanager/journeybackend/repository/TripAuditRepository.java
@@ -0,0 +1,12 @@
+package com.journeymanager.journeybackend.repository;
+
+import com.journeymanager.journeybackend.model.audit.TripAudit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TripAuditRepository extends JpaRepository<TripAudit, Long> {
+
+    List<TripAudit> findByTripIdOrderByTimestampAsc(Long tripId);
+
+}
