8a63d2f96826dbd4a816b8b2d79462f483b6de28
JourneyManager <yklee88@hotmail.com>
2026-03-13 07:09:55 +0800

    Add TripAuditService

---
+++ src/main/java/com/journeymanager/journeybackend/service/TripAuditService.java
@@ -0,0 +1,27 @@
+package com.journeymanager.journeybackend.service;
+
+import com.journeymanager.journeybackend.model.audit.TripAudit;
+import com.journeymanager.journeybackend.repository.TripAuditRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TripAuditService {
+
+    private final TripAuditRepository repository;
+
+    public TripAuditService(TripAuditRepository repository) {
+        this.repository = repository;
+    }
+
+    public void log(Long tripId, String action, String user, String role) {
+
+        TripAudit audit = new TripAudit(
+                tripId,
+                action,
+                user,
+                role
+        );
+
+        repository.save(audit);
+    }
+}
