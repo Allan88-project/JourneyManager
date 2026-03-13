6d77e6fc24ea0f862019d7e0aade260b27572804
JourneyManager <yklee88@hotmail.com>
2026-03-13 14:58:32 +0800

    Fix TripAnalyticsListener dependency injection

--- src/main/java/com/journeymanager/journeybackend/analytics/listener/TripAnalyticsListener.java
+++ src/main/java/com/journeymanager/journeybackend/analytics/listener/TripAnalyticsListener.java
@@ -1,9 +1,12 @@
 package com.journeymanager.journeybackend.analytics.listener;
 
+import com.journeymanager.journeybackend.analytics.dto.AdminAnalyticsResponse;
 import com.journeymanager.journeybackend.analytics.service.AdminAnalyticsService;
-import com.journeymanager.journeybackend.trip.domain.event.TripCompletedEvent;
+import com.journeymanager.journeybackend.analytics.service.AnalyticsBroadcaster;
 import com.journeymanager.journeybackend.trip.domain.event.TripCreatedEvent;
 import com.journeymanager.journeybackend.trip.domain.event.TripStartedEvent;
+import com.journeymanager.journeybackend.trip.domain.event.TripCompletedEvent;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -11,25 +14,47 @@ import org.springframework.stereotype.Component;
 public class TripAnalyticsListener {
 
     private final AdminAnalyticsService analyticsService;
+    private final AnalyticsBroadcaster broadcaster;
 
-    public TripAnalyticsListener(AdminAnalyticsService analyticsService) {
+    public TripAnalyticsListener(
+            AdminAnalyticsService analyticsService,
+            AnalyticsBroadcaster broadcaster
+    ) {
         this.analyticsService = analyticsService;
+        this.broadcaster = broadcaster;
     }
 
     @EventListener
     public void handleTripCreated(TripCreatedEvent event) {
+
         analyticsService.refreshCache();
+
+        AdminAnalyticsResponse analytics =
+                analyticsService.getAnalytics();
+
+        broadcaster.broadcast(analytics);
     }
 
     @EventListener
     public void handleTripStarted(TripStartedEvent event) {
+
         analyticsService.refreshCache();
+
+        AdminAnalyticsResponse analytics =
+                analyticsService.getAnalytics();
+
+        broadcaster.broadcast(analytics);
     }
 
     @EventListener
     public void handleTripCompleted(TripCompletedEvent event) {
+
         analyticsService.refreshCache();
+
+        AdminAnalyticsResponse analytics =
+                analyticsService.getAnalytics();
+
+        broadcaster.broadcast(analytics);
     }
 
 }
-
